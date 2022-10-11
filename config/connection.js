a0324f50c018c2c0217da09534c2248b700c7a4d
VaishaliQA <patelvaishali1890@gmail.com>
2022-10-11 13:40:23 -0700

    updated server.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 let sequelize;
-// Create database connection using sequelize
+// Create database connection using sequelize and set parameters
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
